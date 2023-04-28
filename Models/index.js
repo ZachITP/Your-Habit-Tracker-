9d59df50b5f8d291f2fffaec594b9f475b0e7e5f
Zach Hanson <zachhansonitp@gmail.com>
2023-04-28 10:47:47 -0500

    created MVC file structure

---
+++ Models/index.js
@@ -0,0 +1,32 @@
+// import models
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};

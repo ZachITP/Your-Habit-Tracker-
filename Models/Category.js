62ed54762d937795efcc2e8ef12c459c4d6253e7
sjbossa <scott.bossard@gmail.com>
2023-05-04 20:35:51 -0500

    adding category model

--- Models/Category.js
+++ Models/Category.js
@@ -5,7 +5,21 @@ class Category extends Model {}
   
 Category.init(
     {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      category_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
 
+      },
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'category',
     }
   );
 

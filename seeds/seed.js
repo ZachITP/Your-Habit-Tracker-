65809ca2e1578902fdec66b479a3970452d62be1
palejrey@gmail.com <palejrey@gmail.com>
2023-05-05 14:39:33 -0500

    Seeded data Done by Zac, and seed.js done by Reynaldo

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,24 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Habit, Category } = require('../models');
 
 const userData = require('./userData.json');
+const habitData = require('./habitData.json');
+const categoryData = require('./categoryData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Habit.bulkCreate(habitData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Category.bulkCreate(categoryData, {
     individualHooks: true,
     returning: true,
   });

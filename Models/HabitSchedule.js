93d7009b2f41861509cb15cfd1279fc8a757a4fb
palejrey@gmail.com <palejrey@gmail.com>
2023-05-04 21:13:50 -0500

    Commiting for Zach due to commit issues. All of this is his work.

--- Models/HabitSchedule.js
+++ Models/HabitSchedule.js
@@ -1,12 +1,31 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class HabitSchedule extends Model {}
-  
-HabitSchedule.init(
-    {
 
-    }
-  );
+HabitSchedule.init({
+  day: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  timeOfDay: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    reference: {
+      model: "Users",
+      key: "Id",
+    },
+  },
+  habitid: {
+    type: DataTypes.INTEGER,
+    reference: {
+      model: "Users",
+      key: "Id",
+    },
+  },
+});
 
-  module.exports = HabitSchedule;
+module.exports = HabitSchedule;

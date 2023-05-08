9058c4504b460615ae6e6aac517c9c8589d5488d
Zach Hanson <zachhansonitp@gmail.com>
2023-05-08 14:19:06 -0500

    changed route name

--- controllers/templateRoutes.js
+++ controllers/templateRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User } = require('../Models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
